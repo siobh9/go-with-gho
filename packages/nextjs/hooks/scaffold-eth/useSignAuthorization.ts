f2dcdab0aea73c599f99af2b5364489717518a3f
Siobhán McCaffery <smccaffery9@proton.me>
2025-05-14 16:17:27 -0400

    chore: cleanup pt 2 electric bugaloo

--- packages/nextjs/hooks/scaffold-eth/useSignAuthorization.ts
+++ packages/nextjs/hooks/scaffold-eth/useSignAuthorization.ts
@@ -1,28 +1,20 @@
-import { createWalletClient, http, encodeFunctionData, Authorization, Address } from "viem";
-import { sepolia } from "viem/chains";
 import { useAccount, useSignMessage } from "wagmi";
 
 export const useSignAuthorization = () => {
   const { address: connectedAddress } = useAccount();
-  const { data: signedMessageData, error, signMessage, variables, isSuccess: signingSuccessful } = useSignMessage();
+  const { data: signedMessageData, signMessage, isSuccess: signingSuccessful } = useSignMessage();
 
   const signAuthorization = async () => {
-    const walletClient = createWalletClient({
-      account: connectedAddress ?? '',
-      chain: sepolia,
-      transport: http(),
-    });
-
     const authorizationMessage = {
-        /** Address of the contract to delegate to. */
-        address: walletClient.account.address,
-        /** Chain ID. */
-        chainId: 2,
-        /** Nonce of the EOA to delegate to. */
-        nonce: 1
+      /** Address of the contract to delegate to. */
+      address: connectedAddress,
+      /** Chain ID. */
+      chainId: 2,
+      /** Nonce of the EOA to delegate to. */
+      nonce: 1,
     };
 
-    signMessage({message: JSON.stringify(authorizationMessage)});
+    signMessage({ message: JSON.stringify(authorizationMessage) });
   };
 
   return { signAuthorization, signedMessageData, signingSuccessful };
--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -4,26 +4,11 @@ import {
   ledgerWallet,
   metaMaskWallet,
   rainbowWallet,
-  safeWallet,
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
-import { rainbowkitBurnerWallet } from "burner-connector";
-import * as chains from "viem/chains";
 import scaffoldConfig from "~~/scaffold.config";
 
-const { onlyLocalBurnerWallet, targetNetworks } = scaffoldConfig;
-
-const wallets = [
-  metaMaskWallet,
-  walletConnectWallet,
-  ledgerWallet,
-  coinbaseWallet,
-  rainbowWallet,
-  safeWallet,
-  ...(!targetNetworks.some(network => network.id !== (chains.hardhat as chains.Chain).id) || !onlyLocalBurnerWallet
-    ? [rainbowkitBurnerWallet]
-    : []),
-];
+const wallets = [metaMaskWallet, walletConnectWallet, ledgerWallet, coinbaseWallet, rainbowWallet];
 
 /**
  * wagmi connectors for the wagmi context
