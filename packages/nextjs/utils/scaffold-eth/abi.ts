e0c73bcdc8fa396c882f626c9b9bf492b31c7dfd
Siobhán McCaffery <smccaffery9@proton.me>
2025-05-14 15:04:35 -0400

    chore: add abi file

---
+++ packages/nextjs/utils/scaffold-eth/abi.ts
@@ -0,0 +1,18 @@
+export const abi = [
+  {
+    "type": "function",
+    "name": "initialize",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "pure"
+  },
+  {
+    "type": "function",
+    "name": "ping",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "pure"
+  },
+] as const
+ 
+export const contractAddress = '0x...'
